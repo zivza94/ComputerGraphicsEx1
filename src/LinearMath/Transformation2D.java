be588306c8fcda828de8266af79c370d062553e9
zivza94 <zivza94@gmail.com>
2020-03-26 12:01:17 +0200

    fix Vector.GetLength

--- src/LinearMath/Transformation2D.java
+++ src/LinearMath/Transformation2D.java
@@ -3,12 +3,12 @@ package LinearMath;
 public class Transformation2D {
     public Matrix translate(double deltaX, double deltaY) {
             double[][] array = {{1, 0 , deltaX} , {0, 1, deltaY} , {0, 0 ,1} };
-        Matrix matrix = new Matrix(array, 3);
+        Matrix matrix = new Matrix(array, array.length);
         return matrix;
     }
     public Matrix scale(double a, double b) {
         double[][] array = {{a, 0 , 0} , {0, b, 0} , {0, 0 ,1} };
-        Matrix matrix = new Matrix(array, 3);
+        Matrix matrix = new Matrix(array, array.length);
         return matrix;
     }
     public Matrix rotate(double angle) {
@@ -16,7 +16,7 @@ public class Transformation2D {
         double cos = Math.cos(angle);
         double sin = Math.sin(angle);
         double[][] array = {{cos, sin , 0} , {-sin, cos, 0} , {0, 0 ,1} };
-        Matrix matrix = new Matrix(array, 3);
+        Matrix matrix = new Matrix(array, array.length);
         return matrix;
     }
 }
